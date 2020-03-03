f14916638276c0dc791cbbc41889935573b31e96
Brett Dale <brettdale828@gmail.com>
2020-03-02 19:32:13 -0500

    Creating all lexemes

--- tokenizer.c
+++ tokenizer.c
@@ -4,17 +4,86 @@
  * NOTE: The terms 'token' and 'lexeme' are used interchangeably in this
  *       program.
  *
- * @author Your Name
- * @version current date
+ * @author Brett Dale
+ * @author Julia Januchowski
+ * @version 1.0 (3/2/2020)
  */
 
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include "tokenizer.h"
 
 // global variables
-char *line;             // Global pointer to line of input
+char *line; // Global pointer to line of input
+
+
+/**
+ * This method eliminates all whitespace leading and/or trailing the string passed
+ * @param string string to be trimmed
+ */
+void trim(char *string) {
+    char *ptr = string;
+    int length = strlen(ptr);
+
+    while(isspace(ptr[length - 1])){
+        ptr[--length] = 0;
+    }
+    while(*ptr && isspace(*ptr)){
+        ++ptr, --length;
+    }
+    memmove(string, ptr, length + 1);
+}
+
+/**
+ * This function adds a lexeme to the lexemes array passed in,
+ * then returns the index where it was added
+ * @param lexemes array to add lexeme
+ * @param lexeme lexeme to be added
+ * @param count index of last added lexeme
+ * @return index of next available spot in array
+ */
+int add(Lexeme lexemes[], Lexeme lexeme, int count){
+    lexemes[count] = lexeme;
+    return count + 1;
+}
+
+/**
+ * This function creates all lexemes along with their values and prefixes
+ * and places them into the array passed in
+ * @param lexemes array to hold created lexemes
+ */
+void initialize_lexemes(Lexeme lexemes[]){
+    int count = 0;
+    Lexeme ADD_OP = {"ADD_OP", "+", "an"};
+    Lexeme SUB_OP = {"SUB_OP", "-", "a"};
+    Lexeme MULT_OP = {"MULT_OP", "*", "a"};
+    Lexeme DIV_OP = {"DIV_OP", "/", "a"};
+    Lexeme LEFT_PAREN = {"LEFT_PAREN", "(", "a"};
+    count = add(lexemes, ADD_OP, count);
+    count = add(lexemes, SUB_OP, count);
+    count = add(lexemes, MULT_OP, count);
+    count = add(lexemes, DIV_OP, count);
+    count = add(lexemes, LEFT_PAREN, count);
+    //LEFT OFF HERE 3/2/2020 7:30pm
+}
+
+/**
+ * The function will have a character pointer as an argument.
+ * This pointer will point to a character array (declared in main) that
+ * should contain the last token discovered after the function returns.
+ * The char* that is passed to the get_token function will contain the
+ * next token in line when it, get_token returns. So if I had the line 12=17,
+ * AFTER the first time I ran get_token(var), var would contain the
+ * string ``12'' and the second time it would contain the string ``=''
+ *
+ * @param token_ptr pointer to token passed
+*/
+void get_token(char *token_ptr)
+{
+    // Add code here.
+}
 
 /**
 * add comment
@@ -45,24 +114,20 @@ int main(int argc, char* argv[]) {
         exit(1);
     }
 
-    while (fgets(input_line, LINE, in_file) != NULL)
-    {
-        line = input_line;  // Sets a global pointer to the memory location
-        // where the input line resides.
+    //Brett did this
+    Lexeme lexemes[TSIZE];
+    initialize_lexemes(lexemes);
 
+    while (fgets(input_line, LINE, in_file) != NULL){
+        // Sets a global pointer to the memory location
+        // where the input line resides.
+        line = input_line;
         // Add code here.
+        //printf("%s", line);
 
     }
 
     fclose(in_file);
     fclose(out_file);
     return 0;
-}
-
-/**
-* add javadoc-like comment
-*/
-void get_token(char *token_ptr)
-{
-    // Add code here.
 }
