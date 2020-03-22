02d647cb362556056a04e7a44bb39fdc192a4901
Julia Januchowski <juliajanuchowski@Julias-MacBook-Pro.local>
2020-03-22 15:51:35 -0400

    commenting

--- tokenizer.c
+++ tokenizer.c
@@ -6,7 +6,7 @@
  *
  * @author Brett Dale
  * @author Julia Januchowski
- * @version 1.0 (3/2/2020)
+ * @version 1.0 (3/22/2020)
  */
 
 #include <stdio.h>
@@ -32,15 +32,13 @@ int main(int argc, char* argv[]) {
     FILE  *in_file = NULL;      /* File pointer                     */
     FILE  *out_file = NULL;     /* File pointer                     */
     int   line_count,           /* Number of lines read             */
-    start,                  /* start of new statement           */
-    count;                  /* count of tokens                  */
-
+        start,                  /* start of new statement           */
+        count;                  /* count of tokens                  */
     //If missing all arguments notifies user and gracefully exits
     if (argc != 3) {
         printf("Usage: tokenizer inputFile outputFile\n");
         exit(1);
     }
-
     in_file = fopen(argv[1], "r");
     //if input file can't be read the user is notified and program gracefully exits
     if (in_file == NULL) {
@@ -53,37 +51,37 @@ int main(int argc, char* argv[]) {
         fprintf(stderr, "ERROR: could not open %s for writing\n", argv[2]);
         exit(1);
     }
-    line_count = 1;
-    start = TRUE; //true
+    line_count = 1;  // variable that keeps track of the statements
+    start = TRUE;  // variable that keeps track of whether the current lexeme is the start of a new line
+    //counters for tokens and lexemes
     int curr_token = 0;
     int curr_lexeme = 0;
-
+    // while loop that loops through each line of the input file then writes it to the output file.
     while (fgets(input_line, LINE, in_file) != NULL){
         clear_token_array(token, TSIZE);
-        // Sets a global pointer to the memory location
-        // where the input line resides.
+        // Sets a global pointer to the memory location where the input line resides.
         line = input_line;
-        // Add code here.
+        // while the current lexeme is not a null character get the token and then write it to the file
         while(line[0] != '\0') {
             get_token(token);
-            //write to file
+            // write to file
             curr_lexeme = write_output(token, out_file, line_count, &start, curr_token, curr_lexeme);
+            // if the current token is the start of a new statement set the current counters to true.
             if(start == TRUE){
                 curr_token = 0;
                 curr_lexeme = 0;
-            }
-        }
-
+            } // end if
+        } // end while
+        // if you are at the start a new line add the dashed line and increment line count
         if(token[0] != '\0' && start == TRUE) {
             fprintf(out_file, "\n---------------------------------------------------------\n");
             line_count++;
-        }
-    }
-
+        } // end if
+    } // end while
     fclose(in_file);
     fclose(out_file);
     return 0;
-}
+} // end main
 
 /**
  * The function will have a character pointer as an argument.
@@ -97,33 +95,44 @@ int main(int argc, char* argv[]) {
  * @param token_ptr pointer to token passed
 */
 void get_token(char *token_ptr){
-    // Add code here.
-    // printf("%s", line);
     while (line[0] != '\0') {
+        //if statement that is used to get rid of spaces in a line
         if (!(isspace(line[0]))) {
             *token_ptr = line[0];
+            //if statement that ends a line after reaching a semi colan. otherwise increment the pointer
             if (line[0] == ';'){
                 token_ptr++;
                 *token_ptr = '\0';
             }else{
                 token_ptr++;
-            }
-        }
+            } // end if/else
+        } // end if
         line++;
-    }
-}
+    } // end while
+} // end get_token
 
+/**
+ * this function prints the output to the the output file while also identifying the tokens of each lexeme
+ * @param tokens an array that holds the current input file line
+ * @param output output file
+ * @param line_count the current line / statement count of the file
+ * @param start "boolean" value that holds whether the current token is the start of the current statement
+ * @param curr_token holds the number of the current index of the token array
+ * @param curr_lexeme counter that holds the number of the current lexeme of a statement
+ * @return returns the current lexeme number to be used later if the current lexeme is not the end of a statement
+ */
 int write_output(const char tokens[], FILE *output, int line_count, int *start, int curr_token, int curr_lexeme){
+    //if the current token is the start of a new statement print the correct output
     if(tokens[0] != '\0' && *start == TRUE) {
         fprintf(output, "Statement #%d", line_count);
-    }
+    } // end if
+    // while loop that loops through the tokens until it reaches the null character and prints out the correct output
     while(tokens[curr_token] != '\0') {
-        *start = FALSE;
+        *start = FALSE; // sets the start value to false
         char temp = tokens[curr_token];
-        //switch statement for each case
-        //will probably need LOTS of helper functions :-)
         char digits[TSIZE];
         int curr_digit = 0;
+        //switch statement for each token case
         switch (temp) {
             case '=' :
                 if (tokens[curr_token + 1] == '=') {
@@ -134,7 +143,7 @@ int write_output(const char tokens[], FILE *output, int line_count, int *start, 
                     fprintf(output, "\nLexeme %d is =", curr_lexeme);
                     curr_token++;
                     curr_lexeme++;
-                }
+                } // end if/else
                 break;
             case '<':
                 if (tokens[curr_token + 1] == '=') {
@@ -145,7 +154,7 @@ int write_output(const char tokens[], FILE *output, int line_count, int *start, 
                     fprintf(output, "\nLexeme %d is <", curr_lexeme);
                     curr_token++;
                     curr_lexeme++;
-                }
+                } // end if/else
                 break;
             case '>':
                 if (tokens[curr_token + 1] == '=') {
@@ -156,7 +165,7 @@ int write_output(const char tokens[], FILE *output, int line_count, int *start, 
                     fprintf(output, "\nLexeme %d is >", curr_lexeme);
                     curr_token++;
                     curr_lexeme++;
-                }
+                } // end if/else
                 break;
             case '!':
                 if (tokens[curr_token + 1] == '=') {
@@ -167,7 +176,7 @@ int write_output(const char tokens[], FILE *output, int line_count, int *start, 
                     fprintf(output, "\nLexeme %d is !", curr_lexeme);
                     curr_token++;
                     curr_lexeme++;
-                }
+                } // end if/else
                 break;
             case '+':
                 fprintf(output, "\nLexeme %d is +", curr_lexeme);
@@ -211,11 +220,12 @@ int write_output(const char tokens[], FILE *output, int line_count, int *start, 
                 curr_lexeme++;
                 break;
             case '0' ... '9':
+                // while loop that looks for additional digits
                 while (isdigit(tokens[curr_token])){
                     digits[curr_digit] = tokens[curr_token];
                     curr_digit++;
                     curr_token++;
-                }
+                } // end while
                 fprintf(output, "\nLexeme %d is %s", curr_lexeme, digits);
                 curr_lexeme++;
                 break;
@@ -224,15 +234,21 @@ int write_output(const char tokens[], FILE *output, int line_count, int *start, 
                 fprintf(output,"\nLexical error: not a lexeme");
                 curr_token++;
                 break;
-        }
-    }
+        } // end switch case
+    } // end while
     return curr_lexeme;
-}
+} // end write_output
 
+/**
+ * clears the token array
+ * @param tokens array that holds the current tokens in the array
+ * @param count holds the number of tokens in the array
+ */
 void clear_token_array(char tokens[], int count){
     int i = 0;
+    //loops through the token array
     while(i < count){
         tokens[i] = '\0';
         i++;
-    }
-}
+    } // end while
+} // end clear_token_array
--- tokenizer.h
+++ tokenizer.h
@@ -2,13 +2,14 @@
  * Header file for the tokenizer project
  * @author Brett Dale
  * @author Julia Januchowski
- * @version 1.0 (3/2/2020)
+ * @version 1.0 (3/22/2020)
  */
 /* Constants */
 #define LINE 100
 #define TSIZE 20
 #define TRUE 1
 #define FALSE 0
+
 /* Function prototype headers */
 void get_token(char *);
 int write_output(const char tokens[], FILE *output, int , int *, int, int);
